4ee193d837291df08b397a1e8f5c679e3dc028b1
george.meisinger <george.meisinger@i3-corps.com>
2022-04-12 13:28:18 -0500

    working fft

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import { useEffect, useRef } from 'react';
+import './globals';
+import "p5/lib/addons/p5.sound";
 import p5 from 'p5';
+
+import songFile from '../assets/guitar_jam1.wav';
 
 function Sketch(props) {
     const myRef = useRef(null);
 
     const Sketch = (p) => {
         p.setup = () => {
-            p.canvas = p.createCanvas(p.windowWidth-4, p.windowHeight-4);
+            p.canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+            p.noFill();
+
+            p.song = p.loadSound(songFile);
+            p.env = new p5.Envelope();
+            p.fft = new p5.FFT();
+            p.fft.setInput(p.song);
         }
 
         p.draw = () => {
-            p.background(0);
-            p.fill(255);
-            p.rect(50, 50, 50, 50);
+            p.background(200);
+            let spectrum = p.fft.analyze();
+
+            p.beginShape();
+            for(let i=0;i<spectrum.length;i++) {
+                p.vertex(i*2, p.map(spectrum[i], -100, p.width, p.height, 0));
+            }
+            p.endShape();
         }
 
         p.windowResized = () => {
-            p.resizeCanvas(p.windowWidth-4, p.windowHeight-4);
+            p.resizeCanvas(p.windowWidth, p.windowHeight);
+        }
+
+        p.mousePressed = () => {
+            if(p.song.isPlaying()) {
+                p.song.stop();
+            } else {
+                p.song.play();
+            }
         }
     }
 
