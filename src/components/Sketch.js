6373657a75676ceb59c8004166b7e3aba56a9d18
george.meisinger <george.meisinger@i3-corps.com>
2022-04-15 13:00:17 -0500

    circular fft, playing around

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -10,9 +10,9 @@ function Sketch(props) {
     const myRef = useRef(null);
 
     const Sketch = (p) => {
+
         p.setup = () => {
             p.canvas = p.createCanvas(p.windowWidth, p.windowHeight);
-            p.noFill();
 
             p.song = p.loadSound(songFile);
             p.env = new p5.Envelope();
@@ -21,12 +21,91 @@ function Sketch(props) {
         }
 
         p.draw = () => {
-            p.background(200);
+            p.background(135, 206, 235);
+            //move 0,0 to center
+            p.translate(p.width/2, p.height/2);
+            const r = 100;
+            const ampFactor = .3;
+            const rayAmpFactor = .1;
+            const numRays = 80; //number of sun rays
+            const rayLength = 180;
+
             let spectrum = p.fft.analyze();
 
+            // lets set aside the most active frequencies to use in the sun rays
+            let rayPool = [...spectrum].sort((a,b) => { return a - b; }).slice(0,numRays);
+            const minRay = rayPool[rayPool.length-1];
+            let rays = spectrum.filter((ray) => ray >= minRay);
+
+            // make the rays
+            let rayStep = p.TWO_PI/numRays;
+            let rayTerminals = rays.map((ray, i) => {
+                return { x: (0 - p.cos(i*rayStep)) * (rayLength + ray), y: (0 - p.sin(i*rayStep)) * (rayLength + ray) };
+            });
+
+            // draw the background of rays
+            // p.fill('#f48037');
+            // //p.noStroke();
+            // p.beginShape();
+            // rayTerminals.forEach((terminal) => {
+            //     p.vertex(terminal.x, terminal.y);
+            // });
+            // p.endShape(p.CLOSE);
+
+            //draw the rays
+            // p.stroke(0);
+            // p.strokeWeight(p.width / 500);
+            // rayTerminals.forEach((terminal, i) => {
+            //     p.line(0, 0, terminal.x, terminal.y);
+            // });
+
+            // we want to cutoff the unused end of the spectrum to make it prettier
+            for(let i=spectrum.length;i>=0;i--) { 
+                if(spectrum[i] > 0) {
+                    spectrum = spectrum.slice(2, i);
+                    break;
+                }
+            }
+            let step = p.PI/spectrum.length;
+
+            let halfPoints = spectrum.map((amp, i) => {
+                // get carts from polar coords
+                const angle = step * i;
+                const x = (r + (amp * ampFactor)) * p.sin(angle);
+                const y = (r + (amp * ampFactor)) * p.cos(angle);
+                return { x: x, y: y };
+            });
+
+            let points = [...halfPoints].concat(halfPoints.reverse().map((pair, i) => {
+                return { x: 0 - pair.x, y: pair.y };
+            }));
+
+            // draw big sun
+            p.fill('#f48037');
+            p.noStroke();
+            //p.stroke(0);
+            p.strokeWeight(p.width / 280);
             p.beginShape();
-            for(let i=0;i<spectrum.length;i++) {
-                p.vertex(i*2, p.map(spectrum[i], -100, p.width, p.height, 0));
+            for(let i=0;i<points.length;i++) {
+                p.vertex(points[i].x * 2, points[i].y * 2);
+            }
+            p.endShape();
+
+            //draw the rays
+            // p.stroke(0);
+            // p.strokeWeight(p.width / 500);
+            // rayTerminals.forEach((terminal, i) => {
+            //     p.line(0, 0, terminal.x, terminal.y);
+            // });
+
+            // draw sun
+            p.fill('#fafd0f');
+            p.noStroke();
+            //p.stroke(0);
+            p.strokeWeight(p.width / 280);
+            p.beginShape();
+            for(let i=0;i<points.length;i++) {
+                p.vertex(points[i].x, points[i].y);
             }
             p.endShape();
         }
